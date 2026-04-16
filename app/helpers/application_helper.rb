b5f6cad6b39fa802f8ecb92afdfc5e320e76452f
Sergio <sacordova@miuandes.cl>
2026-04-16 10:36:29 -0400

    commit3

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  def formatted_date(date)
+    date.strftime("%B %d, %Y")
+  end
+
+  def formatted_datetime(datetime)
+    datetime.strftime("%B %d, %Y %H:%M")
+  end
+
+  def appointment_status_text(status)
+    case status
+    when 0
+      "Scheduled"
+    when 1
+      "In Progress"
+    when 2
+      "Completed"
+    when 3
+      "Cancelled"
+    end
+  end
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,4 @@
 class Pet < ApplicationRecord
     belongs_to :owner
-    has_many :appointment
+    has_many :appointments
 end
